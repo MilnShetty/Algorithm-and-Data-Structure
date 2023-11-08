fb038898690fd989833510c47aba6e77059c63b8
MilnShetty <142089656+MilnShetty@users.noreply.github.com>
2023-11-08 15:41:40 +0530

    Add files via upload

---
+++ ADS Lab/Sessional One/heap___sort.py
@@ -0,0 +1,32 @@
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[i] < arr[left]:
+            largest = left
+        if right < n and arr[largest] < arr[right]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+arr = [62, 24, 11, 21, 10]
+print("Original Array:", arr)
+
+
+heap_sorted = arr.copy()
+heap_sort(heap_sorted)
+print("Heap Sort:", heap_sorted)
+
